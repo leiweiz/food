b04a543571613e56d2fe38b2b445edb1339951e8
leiweiz <zhengleiwei@hotmail.com>
2017-07-03 16:45:53 -0400

    added passport strategy: jwt strategy

--- server/routes/auth_router.js
+++ server/routes/auth_router.js
@@ -1,7 +1,11 @@
+require('../services/passport');
+const passport = require('passport');
 const authController = require('../controllers/auth_controller');
 
+const requireAuth = passport.authenticate('jwt', { session: false });
+
 module.exports = (app) => {
-    app.get('/api', authController.test);
+    app.get('/api', requireAuth, authController.test);
 
     app.post('/signup', authController.signup);
 }
--- server/test/app_test.js
+++ server/test/app_test.js
@@ -2,6 +2,7 @@ const assert = require('assert');
 const request = require('supertest');
 const app = require('../app');
 
+/*
 describe('The express app', () => {
     it('handles a GET requests to /api', (done) => {
         request(app)
@@ -13,3 +14,4 @@ describe('The express app', () => {
     });
 
 });
+*/
