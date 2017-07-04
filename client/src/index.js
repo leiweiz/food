076c287914e8af580560c52b92e1e730f822b99f
leiweiz <zhengleiwei@hotmail.com>
2017-07-04 12:02:01 -0400

    add auto login

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
+import { AUTH_USER } from './actions/types';
 
 import App from './components/app';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const token = localStorage.getItem('token');
+if (token) {
+    store.dispatch({ type: AUTH_USER })
+}
 
 ReactDOM.render(
     <Provider store={store}>
